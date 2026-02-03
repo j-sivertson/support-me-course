0d92c6d6d5a6f35f94a6c21bb0b64e8ec1e05e4c
j-sivertson <joshsivertson@outlook.com>
2026-02-03 08:13:33 -0700

    Add light/dark mode toggle component

---
+++ components/ui/light-dark-toggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Button } from "./button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip";
+import { SunIcon, MoonIcon} from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function LightDarkToggle({ className }: { className?: string }){
+    const { setTheme, resolvedTheme } = useTheme();
+    return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild className={className} onClick={() => {
+                    setTheme(resolvedTheme == "light" ? "dark" : "light")
+                }}>
+                    <Button variant="outline">
+                        <SunIcon className="block dark:hidden" />
+                        <MoonIcon className="hidden dark:block"/>
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <span className="hidden dark:inline">Enable light mode</span>
+                    <span className="inline dark:none">Enable dark mode</span>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    );
+}
